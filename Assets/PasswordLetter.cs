fc33f6a6cfa446961923d9689679e1148d8037d2
Caseyfam <robobadnik@gmail.com>
2018-01-15 20:16:48 -0600

    Password system

---
+++ Assets/PasswordLetter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasswordLetter : MonoBehaviour {
+
+    private PasswordSystem system;
+    public string letter;
+
+    private void Awake()
+    {
+        system = GameObject.Find("MenuLogic").GetComponent<PasswordSystem>();
+    }
+
+    public void PassLetter()
+    {
+        system.AddToPassword(letter);
+    }
+}
---
+++ Assets/PasswordSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasswordSystem : MonoBehaviour {
+
+    private string password = "";
+
+    public void EnterPassword()
+    {
+        // Load to various scenes here
+    }
+
+    public void AddToPassword(string letter)
+    {
+        password += letter;
+        Debug.Log(password);
+    }
+
+    public void ClearPassword()
+    {
+        password = "";
+    }
+}
