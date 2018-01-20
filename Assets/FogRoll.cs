528ac120eda7c03c29fe927f26a8bfa85fb30615
Caseyfam <robobadnik@gmail.com>
2018-01-19 19:56:08 -0600

    Shrine background complete

---
+++ Assets/FogRoll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogRoll : MonoBehaviour {
+
+    public Vector3 leftExit, rightEnter;
+    public float rollStep;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, leftExit, rollStep);
+        if (transform.position == leftExit)
+        {
+            transform.position = rightEnter;
+        }	
+	}
+}
