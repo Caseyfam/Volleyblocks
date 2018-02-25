4fa60690a1000bedae6dffba54ed00a7c370d3c7
Caseyfam <robobadnik@gmail.com>
2018-02-25 17:21:19 -0600

    Lab Scene
    Initial implementation

---
+++ Assets/LabTankBob.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabTankBob : MonoBehaviour {
+
+    public float direction = 0.1f;
+    public float waitTime = 1f;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        StartCoroutine(FlipDir(waitTime));
+        waitTime *= 2;
+	}
+
+    IEnumerator FlipDir(float time)
+    {
+        yield return new WaitForSeconds(time);
+        direction = -direction;
+        StartCoroutine(FlipDir(waitTime));
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + direction, transform.position.z);
+    }
+}
