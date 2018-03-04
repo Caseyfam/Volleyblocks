6092292846ab15254907905b5e4e6431384da9ed
Caseyfam <robobadnik@gmail.com>
2018-03-04 13:38:02 -0600

    Revert "Revert "Added volley internal scene""
    This reverts commit b350eb6f7347ba6d60e45e8299a8d4f69ca2ce1d.

--- Assets/Scripts/SetScene.cs
+++ Assets/Scripts/SetScene.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class SetScene : MonoBehaviour {
 
     SelectedScene selectedScene;
-	// Use this for initialization
+    // Use this for initialization
+    bool scenesSetActive = false;
+
+    public GameObject[] allObjects;
 
     [System.Serializable]
     public class Scenes
     {
-        public GameObject beachScene, shrineScene, foyerScene;
+        public GameObject beachScene, shrineScene, foyerScene, labScene;
     }
     public Scenes scenes = new Scenes();
 
@@ -41,7 +44,35 @@ public class SetScene : MonoBehaviour {
             case "foyer":
                 scenes.foyerScene.SetActive(true);
                 break;
+            case "lab":
+                scenes.labScene.SetActive(true);
+                break;
+            case "glitch":
+                StartCoroutine(WaitToGlitch(0.3f));
+                break;
         }
+    }
+
+
+    IEnumerator WaitToGlitch(float time)
+    {
+        if (!scenesSetActive)
+        {
+            scenes.beachScene.SetActive(true);
+            scenes.labScene.SetActive(true);
+            scenes.foyerScene.SetActive(true);
+            scenes.shrineScene.SetActive(true);
+            scenesSetActive = true;
+        }
+        yield return new WaitForSeconds(time);
+        GameObject selected = allObjects[Random.Range(0, allObjects.Length - 1)];
+        selected.SetActive(!selected.activeSelf);
+        try
+        {
+            selected.GetComponent<SpriteRenderer>().sortingOrder = -5;
+        }
+        catch { }
+        StartCoroutine(WaitToGlitch(time));
     }
 	
 }
